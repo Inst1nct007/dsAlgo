7f0f9455b7ee403727cef0375b6801bbfe761ba0
Inst1nct007 <94550979+Inst1nct007@users.noreply.github.com>
2023-05-18 23:05:29 +0530

    implemented Circular Doubly Linked List Class

--- python/doubly_linked_list.py
+++ python/doubly_linked_list.py
@@ -66,10 +66,98 @@ class DoublyLinkedList:
         finally:
             del lastNode
 
+    def search(self):
+        pass
+
     def __str__(self) -> str:
         nodes = []
         node = self.head.next
         while node is not None:
             nodes.append(str(node.data))
             node = node.next
-        return " <-> ".join(nodes)
+        return " <=> ".join(nodes)
+
+
+class CircularDoublyLinkedList(DoublyLinkedList):
+    def __init__(self, *args) -> None:
+        self.head = Node(None)
+        self.head.next = None
+        self.head.prev = None
+        prevNode = self.head
+        for i in args:
+            newNode = Node(i)
+            newNode.prev = prevNode
+            prevNode.next = newNode
+            newNode.next = self.head.next
+            prevNode = newNode
+
+    def push(self, data):
+        newNode = Node(data)
+        lastNode = self.head.next
+        while lastNode.next is not self.head.next:
+            lastNode = lastNode.next
+        newNode.prev = lastNode
+        lastNode.next = newNode
+        newNode.next = self.head.next
+
+    def insert(self, data, pos):
+        newNode = Node(data)
+        prevNode = self.head.next
+        while prevNode.next is not self.head.next:
+            prevNode = prevNode.next
+        for i in range(1, pos):
+            prevNode = prevNode.next
+        newNode.next = prevNode.next
+        newNode.prev = prevNode
+        prevNode.next = newNode
+        if pos == 1:
+            self.head.next = newNode
+
+    def pop(self):
+        lastNode = self.head
+        prevNode = lastNode
+        i = 1
+        while lastNode.next is not self.head.next or i == 1:
+            i += 1
+            prevNode = lastNode
+            lastNode = lastNode.next
+
+        prevNode.next = self.head.next
+        if lastNode is self.head.next:
+            del prevNode
+            self.head.next = None
+        del lastNode
+
+    def popAt(self, pos):
+        prevNode = self.head.next
+        while prevNode.next is not self.head.next:
+            prevNode = prevNode.next
+        lastNode = prevNode.next
+        for i in range(1, pos):
+            prevNode = lastNode
+            lastNode = lastNode.next
+        prevNode.next = lastNode.next
+        lastNode.next.prev = prevNode
+        if pos == 1:
+            if prevNode == lastNode:
+                self.head.next = None
+            else:
+                self.head.next = prevNode.next
+        del lastNode
+
+    def __str__(self) -> str:
+        nodes = []
+        node = self.head.next
+        i = 1
+        try:
+            while node.next is not self.head.next:
+                nodes.append(str(node.data))
+                node = node.next
+            nodes.append(str(node.data))
+        except:
+            pass
+        return " <=> ".join(nodes)
+
+
+newLinkedList = CircularDoublyLinkedList(1, 2, 3)
+print(newLinkedList)
