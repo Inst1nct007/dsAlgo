770ae1541e8e03447d9f0ee3ae9ea3bc9f0152ff
Inst1nct007 <94550979+Inst1nct007@users.noreply.github.com>
2023-05-18 17:49:23 +0530

    Created __init__() constructor and push()
    The constructor creates new nodes. The push() appends new nodes to the end of the linked list.

--- python/doubly_linked_list.py
+++ python/doubly_linked_list.py
@@ -9,9 +9,23 @@ class DoublyLinkedList:
     def __init__(self, *args) -> None:
         self.head = Node(None)
         self.head.prev = None
+        self.head.next = None
+        prevNode = self.head
+        for i in args:
+            newNode = Node(i)
+            newNode.prev = prevNode
+            prevNode.next = newNode
+            newNode.next = None
+            prevNode = newNode
 
-    def push(self):
-        pass
+    def push(self, data):
+        newNode = Node(data)
+        lastNode = self.head
+        while lastNode.next is not None:
+            lastNode = lastNode.next
+        newNode.prev = lastNode
+        lastNode.next = newNode
+        newNode.next = None
 
     def pushAt(self):
         pass
@@ -26,4 +40,16 @@ class DoublyLinkedList:
         pass
 
     def __str__(self) -> str:
-        return "pass"
+        nodes = []
+        node = self.head.next
+        while node is not None:
+            nodes.append(str(node.data))
+            node = node.next
+        return " <-> ".join(nodes)
+
+
+newLinkedList = DoublyLinkedList(2, 3, 5, "a", "c")
+print(newLinkedList)
+newLinkedList.push("d")
+newLinkedList.push("f")
+print(newLinkedList)
