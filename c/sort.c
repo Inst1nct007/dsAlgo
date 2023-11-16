bcf4b754c6275a67ebb9d398f10c0769edeeb0e8
Inst1nct007 <94550979+Inst1nct007@users.noreply.github.com>
2023-11-16 17:05:42 +0530

    Added merge sort

--- c/sort.c
+++ c/sort.c
@@ -4,11 +4,13 @@
 int* selectionSort(int* arr, int size);
 int* bubbleSort(int* arr, int size);
 int* insertionSort(int* arr, int size);
+void mergeSort(int arr[], int left, int right);
+void merge(int arr[], int left, int mid, int right);
 
 void main() {
-    int* arr = allocateArray();
-    int* sortedArr = insertionSort(arr + 1, *arr);
-    printArray(sortedArr, *arr);
+    int* arr = allocateArray(); // returns an array arr[0] is always the size of arr, the actual array starts from arr[1]
+    mergeSort(arr + 1, 0, *arr - 1);
+    printArray(arr + 1, *arr);
 }
 
 int* selectionSort(int* arr, int size) {
@@ -53,3 +55,51 @@ int* insertionSort(int* arr, int size) {
     }
     return arr;
 }
+
+void mergeSort(int arr[], int left, int right) 
+{ 
+    if (left < right) { 
+        int mid = left + (right - left) / 2; 
+        mergeSort(arr, left, mid); 
+        mergeSort(arr, mid + 1, right); 
+        merge(arr, left, mid, right); 
+    } 
+}
+
+void merge(int arr[], int left, int mid, int right) 
+{ 
+    int i, j, k; 
+    int sizeOfLeft = mid - left + 1; 
+    int sizeOfRight = right - mid;
+  
+    int L[sizeOfLeft], R[sizeOfRight]; 
+  
+    for (i = 0; i < sizeOfLeft; i++) 
+        L[i] = arr[left + i]; 
+    for (j = 0; j < sizeOfRight; j++) 
+        R[j] = arr[mid + 1 + j]; 
+    i = 0; 
+    j = 0; 
+    k = left; 
+    while (i < sizeOfLeft && j < sizeOfRight) { 
+        if (L[i] <= R[j]) { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+    while (i < sizeOfLeft) { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+    while (j < sizeOfRight) { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+} 
