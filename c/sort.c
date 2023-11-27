92fa52544f3b44d323813615284f81695ed7896d
Inst1nct007 <94550979+Inst1nct007@users.noreply.github.com>
2023-11-27 09:54:10 +0530

    Added Heap Sort using Max Heap
    Added createMaxHeap(), insertMaxHeap(), heapify(), heapSort().

--- c/sort.c
+++ c/sort.c
@@ -7,11 +7,90 @@ int* insertionSort(int* arr, int size);
 void mergeSort(int arr[], int left, int right);
 void merge(int arr[], int left, int mid, int right);
 void quickSort(int* arr, int start, int pivot);
+void createMaxHeap(int* arr, int size);
+int insertMaxHeap(int* arr, int size, int element);
+void heapSort(int* arr, int size);
+void heapify(int* arr, int size);
 
 void main() {
     int* arr = allocateArray(); // returns an array arr[0] is always the size of arr, the actual array starts from arr[1]
-    quickSort(arr + 1, 0, *arr);
-    printArray(arr+1, *arr);
+    createMaxHeap(arr + 1, *arr);
+    *arr = insertMaxHeap(arr + 1, *arr, 33);
+    *arr = insertMaxHeap(arr + 1, *arr, 31);
+    heapSort(arr + 1, *arr);
+    printArray(arr + 1, *arr);
+}
+
+void heapSort(int* arr, int size) {
+    int i = 0;
+    int temp = size;
+    while(i < size - 1) {
+        heapify(arr, temp);
+        temp--;
+        i++;
+    }
+}
+
+void heapify(int* arr, int size) { // Swaps the root element with the last, then creates max heap accordingly
+    arr[0] += arr[size - 1];
+    arr[size - 1] = arr[0] - arr[size - 1];
+    arr[0] -= arr[size - 1];
+    size--;
+    int i = size - 1;
+    while(i >= 0) {
+        if(arr[i] < arr[i * 2 + 1] && i * 2 + 1 < size) {
+            arr[i] += arr[i * 2 + 1];
+            arr[i * 2 + 1] = arr[i] - arr[i * 2 + 1];
+            arr[i] -= arr[i * 2 + 1];
+            i = i * 2 + 1;
+        }
+        else if(arr[i] < arr[i * 2 + 2] && i * 2 + 2 < size) {
+            arr[i] += arr[i * 2 + 2];
+            arr[i * 2 + 2] = arr[i] - arr[i * 2 + 2];
+            arr[i] -= arr[i * 2 + 2];
+            i = i * 2 + 2;
+        } else {
+            i--;
+        }
+    }
+}
+
+int insertMaxHeap(int* arr, int size, int element) { // Inserts an element in the max heap
+    int i = size;
+    arr[size] = element;
+    while(i >= 0) {
+        if(arr[i] < arr[(i * 2) + 1] && i * 2 + 1 < size + 1) {
+            arr[i] += arr[(i * 2) + 1];
+            arr[(i * 2) + 1] = arr[i] - arr[(i * 2) + 1];
+            arr[i] -= arr[(i * 2) + 1];
+        } else if (arr[i] < arr[i * 2 + 2] && i * 2 + 2 < size + 1) {
+            arr[i] += arr[i * 2 + 2];
+            arr[i * 2 + 2] = arr[i] - arr[i * 2 + 2];
+            arr[i] -= arr[i * 2 + 2];
+        } else {
+            i--;
+        }
+    }
+    return size + 1;
+}
+
+void createMaxHeap(int* arr, int size) { // Creates the max heap with an array
+    int i = 0;
+    while(i < size) {
+        if(arr[i] < arr[i * 2 + 1] && i * 2 + 1 < size) {
+            arr[i] += arr[i * 2 + 1];
+            arr[i * 2 + 1] = arr[i] - arr[i * 2 + 1];
+            arr[i] -= arr[i * 2 + 1];
+            i = i / 2;
+        } else if (arr[i] < arr[i * 2 + 2] && i * 2 + 2 < size) {
+            arr[i] += arr[i * 2 + 2];
+            arr[i * 2 + 2] = arr[i] - arr[i * 2 + 2];
+            arr[i] -= arr[i * 2 + 2];
+            i = i / 2;
+        } else {
+            i++;
+        }
+    }
 }
 
 void quickSort(int* arr, int start, int end) {
